afa41cb73a3aa88f874fb34cab0c1f8f0802e3e9
shashi1049 <shashi290396@gmail.com>
2023-10-08 22:58:49 +0530

    Excep

---
+++ Log4jProject/src/main/java/com/log4j/exception/EmployeeExcep.java
@@ -0,0 +1,16 @@
+package com.log4j.exception;
+
+public class EmployeeExcep extends Exception{
+
+	public EmployeeExcep() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EmployeeExcep(String msg) {
+		super(msg);
+		
+	}
+	
+
+}
